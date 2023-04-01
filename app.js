c3ecf6129167960ff70ec4079344a32dc2ba6179
Harsh Pandit <harsh.pandit2004@gmail.com>
2023-04-01 21:54:57 +0530

    access control origin middleware

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const corsOptions = {
 };
 app.options("*", cors(corsOptions));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //mongo db block
 
 mongoose.set('strictQuery', true);
